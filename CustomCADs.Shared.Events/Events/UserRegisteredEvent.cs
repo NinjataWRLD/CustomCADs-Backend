17a937b23ab50299c1630d9c70b3cd2a2238cedb
Ivan Angelov <ivanangelov414@gmail.com>
2024-10-26 12:42:37 +0300

    Added a Shared.Events project

---
+++ CustomCADs.Shared.Events/Events/UserRegisteredEvent.cs
@@ -0,0 +1,10 @@
+﻿namespace CustomCADs.Shared.Events.Events;
+
+public class UserRegisteredEvent
+{
+    public required string Role { get; set; }
+    public required string Username { get; set; }
+    public required string Email { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+}
