9248bfa1d1d445a4a013a706942e8bec0f0f6b61
Ivan Angelov <ivanangelov414@gmail.com>
2024-11-14 18:25:07 +0200

    Added Refit for Speedy's API

---
+++ CustomCADs.Shared.Speedy/DependencyInjection.cs
@@ -0,0 +1,10 @@
+﻿using CustomCADs.Shared.Speedy.ShipmentService;
+using Refit;
+
+#pragma warning disable IDE0130
+namespace Microsoft.Extensions.DependencyInjection;
+
+public static class DependencyInjection
+{
+    private const string BASE_URL = "https://api.speedy.bg/v1";
+}
---
+++ CustomCADs.Shared.Speedy/ShipmentService/IShipmentService.cs
@@ -0,0 +1,6 @@
+﻿namespace CustomCADs.Shared.Speedy.ShipmentService;
+
+public interface IShipmentService
+{
+    
+}
