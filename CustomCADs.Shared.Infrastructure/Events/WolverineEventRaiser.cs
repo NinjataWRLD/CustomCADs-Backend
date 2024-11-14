0c637bf5ef535e67ffb8e0584c416f267487585e
Ivan Angelov <ivanangelov414@gmail.com>
2024-11-15 00:13:53 +0200

    Updated to .NET 9 (Wolverine doesn't support it yet, so I commented out the WolverineEventRaiser implementation)

--- CustomCADs.Shared.Infrastructure/Events/WolverineEventRaiser.cs
+++ CustomCADs.Shared.Infrastructure/Events/WolverineEventRaiser.cs
@@ -3,8 +3,8 @@ using CustomCADs.Shared.Core.Common.Events;
 
 namespace CustomCADs.Shared.Infrastructure.Events;
 
-public class WolverineEventRaiser(Wolverine.IMessageBus bus) : IEventRaiser
+public class WolverineEventRaiser(/*Wolverine.IMessageBus bus*/) : IEventRaiser
 {
     public async Task RaiseAsync<TEvent>(TEvent @event) where TEvent : BaseEvent
-        => await bus.PublishAsync(@event);
+    { }//=> await bus.PublishAsync(@event);
 }
