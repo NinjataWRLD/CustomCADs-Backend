d8308f4c03b9ded5a0fee1350fe232158c4188b3
Ivan Angelov <ivanangelov414@gmail.com>
2024-11-21 23:44:59 +0200

    Defined Speedy Services Service around Services Endpoints

---
+++ CustomCADs.Shared.Speedy/Services/Services/Mapper.cs
@@ -0,0 +1,47 @@
+﻿using CustomCADs.Shared.Speedy.API.Dtos.CourierService;
+using CustomCADs.Shared.Speedy.API.Dtos.ExtendedCourierService;
+using CustomCADs.Shared.Speedy.Services.Services.Models;
+
+namespace CustomCADs.Shared.Speedy.Services.Services;
+
+public static class Mapper
+{
+    public static CourierServiceModel ToModel(this CourierServiceDto model)
+        => new(
+            Id: model.Id,
+            Name: model.Name,
+            NameEn: model.NameEn,
+            CargoType: model.CargoType,
+            RequireParcelSize: model.RequireParcelSize,
+            RequireParcelWeight: model.RequireParcelWeight,
+            AdditionalServices: model.AdditionalServices.ToModel()
+        );
+
+    public static AdditionalCourierServicesModel ToModel(this AdditionalCourierServicesDto model)
+        => new(
+            CodAllowance: model.Cod.Allowance,
+            ObpdAllowance: model.Obpd.Allowance,
+            DeclaredValueAllowance: model.DeclaredValue.Allowance,
+            FixedTimeDeliveryAllowance: model.FixedTimeDelivery.Allowance,
+            SpecialDeliveryAllowance: model.SpecialDelivery.Allowance,
+            DeliveryToFloorAllowance: model.DeliveryToFloor.Allowance,
+            RodAllowance: model.Rod.Allowance,
+            ReturnReceiptAllowance: model.ReturnReceipt.Allowance,
+            SwapAllowance: model.Swap.Allowance,
+            RopAllowance: model.Rop.Allowance,
+            ReturnVoucherAllowance: model.ReturnVoucher.Allowance
+        );
+
+    public static ExtendedCourierServiceModel ToModel(this ExtendedCourierServiceDto model)
+        => new(
+            Deadline: model.Deadline,
+            Id: model.Id,
+            Name: model.Name,
+            NameEn: model.NameEn,
+            CargoType: model.CargoType,
+            RequireParcelSize: model.RequireParcelSize,
+            RequireParcelWeight: model.RequireParcelWeight,
+            AdditionalServices: model.AdditionalServices.ToModel()
+        );
+
+}
---
+++ CustomCADs.Shared.Speedy/Services/Services/Models/CourierServiceModel.cs
@@ -0,0 +1,11 @@
+﻿namespace CustomCADs.Shared.Speedy.Services.Services.Models;
+
+public record CourierServiceModel(
+    int Id,
+    string Name,
+    string NameEn,
+    CargoType CargoType,
+    bool RequireParcelWeight,
+    bool RequireParcelSize,
+    AdditionalCourierServicesModel AdditionalServices
+);
