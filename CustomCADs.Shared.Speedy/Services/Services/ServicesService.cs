5311b007007715fdd72c2817df2cb9416619a4ec
Ivan Angelov <ivanangelov414@gmail.com>
2024-11-21 23:44:59 +0200

    Defined Speedy Services Service around Services Endpoints

---
+++ CustomCADs.Shared.Speedy/Services/Services/ServicesService.cs
@@ -0,0 +1,43 @@
+﻿using CustomCADs.Shared.Speedy.API.Endpoints.ServicesEndpoints;
+using CustomCADs.Shared.Speedy.Models;
+using CustomCADs.Shared.Speedy.Models.Calculation;
+using CustomCADs.Shared.Speedy.Models.Calculation.Recipient;
+using CustomCADs.Shared.Speedy.Models.Calculation.Sender;
+using CustomCADs.Shared.Speedy.Services.Services.Models;
+
+namespace CustomCADs.Shared.Speedy.Services.Services;
+
+using static Constants;
+
+public class ServicesService(IServicesEndpoints services)
+{
+    public async Task<CourierServiceModel[]> Services(DateOnly date, AccountModel account, CancellationToken ct = default)
+    {
+        var response = await services.Services(new(
+            UserName: account.Username,
+            Password: account.Password,
+            Language: account.Language,
+            ClientSystemId: account.ClientSystemId,
+            Date: date.ToString(DateFormat)
+        ), ct).ConfigureAwait(false);
+
+        response.Error.EnsureNull();
+        return [.. response.Services.Select(s => s.ToModel())];
+    }
+
+    public async Task<ExtendedCourierServiceModel[]> DestinationServices(DateOnly date, CalculationRecipientModel recipient, CalculationSenderModel? sender, AccountModel account, CancellationToken ct = default)
+    {
+        var response = await services.DestinationServices(new(
+            UserName: account.Username,
+            Password: account.Password,
+            Language: account.Language,
+            ClientSystemId: account.ClientSystemId,
+            Date: date.ToString(DateFormat),
+            Recipient: recipient.ToDto(),
+            Sender: sender?.ToDto()
+        ), ct).ConfigureAwait(false);
+
+        response.Error.EnsureNull();
+        return [.. response.Services.Select(s => s.ToModel())];
+    }
+}
