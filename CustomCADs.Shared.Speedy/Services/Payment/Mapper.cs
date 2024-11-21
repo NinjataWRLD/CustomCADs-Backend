6ef610d1ebd293b53441f46e4380fd079d786553
Ivan Angelov <ivanangelov414@gmail.com>
2024-11-21 15:50:51 +0200

    Defined Speedy Payment Service around Payment Endpoints

---
+++ CustomCADs.Shared.Speedy/Services/Payment/Mapper.cs
@@ -0,0 +1,36 @@
+﻿using CustomCADs.Shared.Speedy.API.Dtos.Payout;
+using CustomCADs.Shared.Speedy.Services.Payment.Models;
+
+namespace CustomCADs.Shared.Speedy.Services.Payment;
+
+public static class Mapper
+{
+    public static PayoutModel ToModel(this PayoutDto dto)
+        => new(
+            Date: DateOnly.Parse(dto.Date),
+            DocId: dto.DocId,
+            DocType: dto.DocType,
+            PaymentType: dto.PaymentType,
+            Payee: dto.Payee,
+            Currency: dto.Currency,
+            Amount: dto.Amount,
+            Details: [.. dto.Details.Select(d => d.ToModel())]
+        );
+
+    public static PayoutDetailsModel ToModel(this PayoutDetailsDto dto)
+        => new(
+            LineNo: dto.LineNo,
+            ShipmentId: dto.ShipmentId,
+            PickupDate: DateOnly.Parse(dto.PickupDate),
+            PrimaryShipmentPickupDate: DateOnly.Parse(dto.PrimaryShipmentPickupDate),
+            DeliveryDate: DateOnly.Parse(dto.DeliveryDate),
+            Sender: dto.Sender,
+            Recipient: dto.Recipient,
+            Note: dto.Note,
+            Ref1: dto.Ref1,
+            Ref2: dto.Ref2,
+            Currency: dto.Currency,
+            Order: dto.Order,
+            Amount: dto.Amount
+        );
+}
---
+++ CustomCADs.Shared.Speedy/Services/Payment/PaymentService.cs
@@ -0,0 +1,26 @@
+﻿using CustomCADs.Shared.Speedy.API.Endpoints.PaymentEndpoints;
+using CustomCADs.Shared.Speedy.Models;
+using CustomCADs.Shared.Speedy.Services.Payment.Models;
+
+namespace CustomCADs.Shared.Speedy.Services.Payment;
+
+using static Constants;
+
+public class PaymentService(IPaymentEndpoints payment)
+{
+    public async Task<PayoutModel[]> Payout((DateTime From, DateTime To, bool IncludeDetails) model, AccountModel account, CancellationToken ct = default)
+    {
+        var response = await payment.Payout(new(
+            UserName: account.Username,
+            Password: account.Password,
+            Language: account.Language,
+            ClientSystemId: account.ClientSystemId,
+            FromDate: model.From.ToString(DateTimeFormat),
+            ToDate: model.To.ToString(DateTimeFormat),
+            IncludeDetails: model.IncludeDetails
+        ), ct).ConfigureAwait(false);
+
+        response.Error?.EnsureNull();
+        return [.. response.Payouts.Select(p => p.ToModel())];
+    }
+}
