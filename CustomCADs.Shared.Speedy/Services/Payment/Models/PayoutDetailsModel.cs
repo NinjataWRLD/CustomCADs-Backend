4ebd73591ecac3f42bae637ef560aadeae4ac61e
Ivan Angelov <ivanangelov414@gmail.com>
2024-11-21 15:50:51 +0200

    Defined Speedy Payment Service around Payment Endpoints

---
+++ CustomCADs.Shared.Speedy/Services/Payment/Models/PayoutDetailsModel.cs
@@ -0,0 +1,17 @@
+﻿namespace CustomCADs.Shared.Speedy.Services.Payment.Models;
+
+public record PayoutDetailsModel(
+    int LineNo,
+    string ShipmentId,
+    DateOnly PickupDate,
+    DateOnly PrimaryShipmentPickupDate,
+    DateOnly DeliveryDate,
+    string Sender,
+    string Recipient,
+    string Note,
+    string Ref1,
+    string Ref2,
+    string Currency,
+    long Order,
+    double Amount
+);
---
+++ CustomCADs.Shared.Speedy/Services/Payment/Models/PayoutModel.cs
@@ -0,0 +1,12 @@
+﻿namespace CustomCADs.Shared.Speedy.Services.Payment.Models;
+
+public record PayoutModel(
+    DateOnly Date,
+    long DocId,
+    ProcessingType DocType,
+    PaymentType PaymentType,
+    string Payee,
+    string Currency,
+    double Amount,
+    PayoutDetailsModel[] Details
+);
