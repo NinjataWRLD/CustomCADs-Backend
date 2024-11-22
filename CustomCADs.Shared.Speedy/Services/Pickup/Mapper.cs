33c4d4cd9b41cdefc19bdef3a6f83c5cd1cb17e5
Ivan Angelov <ivanangelov414@gmail.com>
2024-11-22 16:16:15 +0200

    Defined Speedy Pickup Service around Pickup Endpoints

---
+++ CustomCADs.Shared.Speedy/Services/Pickup/Mapper.cs
@@ -0,0 +1,15 @@
+﻿using CustomCADs.Shared.Speedy.API.Dtos.PickupOrder;
+using CustomCADs.Shared.Speedy.Services.Pickup.Models;
+
+namespace CustomCADs.Shared.Speedy.Services.Pickup;
+
+public static class Mapper
+{
+    public static PickupOrderModel ToModel(this PickupOrderDto dto)
+        => new(
+            Id: dto.Id,
+            ShipmentIds: dto.ShipmentIds,
+            PickupPeriodFrom: dto.PickupPeriodFrom is not null ? DateTime.Parse(dto.PickupPeriodFrom) : null,
+            PickupPeriodTo: dto.PickupPeriodTo is not null ? DateTime.Parse(dto.PickupPeriodTo) : null
+        );
+}
