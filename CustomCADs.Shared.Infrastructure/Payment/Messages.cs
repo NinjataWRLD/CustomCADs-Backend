dbb627c60e1496f2d0dedd3d087fc37ac8497767
Ivan Angelov <ivanangelov414@gmail.com>
2024-12-02 00:58:44 +0200

    Refined Payment Service

---
+++ CustomCADs.Shared.Infrastructure/Payment/Messages.cs
@@ -0,0 +1,13 @@
+﻿namespace CustomCADs.Shared.Infrastructure.Payment;
+
+public static class Messages
+{
+    public const string SuccessfulPayment = "Payment was successful.";
+    public const string CanceledPayment = "Payment was canceled. Please try again or use a different payment method.";
+    public const string ProcessingPayment = "Payment is processing. Please wait for confirmation.";
+    public const string SuccessfulPaymentCapture = "Payment captured successfully.";
+    public const string FailedPaymentCapture = "Failed to capture payment.";
+    public const string FailedPaymentMethod = "Payment failed. Please try another payment method.";
+    public const string FailedPayment = "Payment requires further action.";
+    public const string UnhandledPayment = "Unhandled payment status: {0}";
+}
