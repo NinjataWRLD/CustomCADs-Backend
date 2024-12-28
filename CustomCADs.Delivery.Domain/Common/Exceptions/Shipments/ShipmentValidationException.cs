bcc4a26c330f8185736308a6c9ee3dd5c34295ac
Ivan Angelov <ivanangelov414@gmail.com>
2024-12-28 21:45:40 +0200

    Updated Delivery module

--- CustomCADs.Delivery.Domain/Common/Exceptions/Shipments/ShipmentValidationException.cs
+++ CustomCADs.Delivery.Domain/Common/Exceptions/Shipments/ShipmentValidationException.cs
@@ -11,6 +11,9 @@ public class ShipmentValidationException : BaseException
 
     public static ShipmentValidationException General(Exception? inner = default)
         => new(string.Format(Validation, "a", "Shipment"), inner);
+    
+    public static ShipmentValidationException NotNull(string property, Exception? inner = default)
+        => new(string.Format(NonNullValidation, "A", "Shipment", property), inner);
 
     public static ShipmentValidationException Custom(string message, Exception? inner = default)
         => new(message, inner);
