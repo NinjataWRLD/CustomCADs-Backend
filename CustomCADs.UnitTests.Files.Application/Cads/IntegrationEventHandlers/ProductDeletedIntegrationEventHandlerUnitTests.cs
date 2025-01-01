819f0a27cab7a4f760a342ea8262c4f4e586a588
Ivan Angelov <ivanangelov414@gmail.com>
2025-01-01 23:52:49 +0200

    Updated Files unit tests

---
+++ CustomCADs.UnitTests.Files.Application/Cads/IntegrationEventHandlers/ProductDeletedIntegrationEventHandlerUnitTests.cs
@@ -0,0 +1,80 @@
+﻿using CustomCADs.Files.Application.Cads.IntegrationEventHandlers;
+using CustomCADs.Files.Domain.Cads.Reads;
+using CustomCADs.Files.Domain.Common;
+using CustomCADs.Shared.Application.Storage;
+using CustomCADs.Shared.IntegrationEvents.Files;
+
+namespace CustomCADs.UnitTests.Files.Application.Cads.IntegrationEventHandlers;
+
+public class ProductDeletedIntegrationEventHandlerUnitTests : CadsBaseUnitTests
+{
+    private readonly ICadReads reads = Substitute.For<ICadReads>();
+    private readonly IWrites<Cad> writes = Substitute.For<IWrites<Cad>>();
+    private readonly IUnitOfWork uow = Substitute.For<IUnitOfWork>();
+    private readonly IStorageService storage = Substitute.For<IStorageService>();
+    private static readonly Cad cad = CreateCad();
+
+    public ProductDeletedIntegrationEventHandlerUnitTests()
+    {
+        reads.SingleByIdAsync(id, true, ct).Returns(cad);
+    }
+
+    [Fact]
+    public async Task Handle_ShouldCallDatabase_WhenCadExists()
+    {
+        // Arrange
+        ProductDeletedIntegrationEvent ie = new(
+            Id: default,
+            ImageId: default,
+            CadId: id
+        );
+        ProductDeletedIntegrationEventHandler handler = new(reads, writes, uow, storage);
+
+        // Act
+        await handler.Handle(ie);
+
+        // Assert
+        await reads.Received(1).SingleByIdAsync(id, true, ct);
+        writes.Received(1).Remove(cad);
+        await uow.Received(1).SaveChangesAsync();
+    }
+
+    [Fact]
+    public async Task Handle_ShouldCallStorage_WhenCadExists()
+    {
+        // Arrange
+        ProductDeletedIntegrationEvent ie = new(
+            Id: default,
+            ImageId: default,
+            CadId: id
+        );
+        ProductDeletedIntegrationEventHandler handler = new(reads, writes, uow, storage);
+
+        // Act
+        await handler.Handle(ie);
+
+        // Assert
+        await storage.Received(1).DeleteFileAsync(cad.Key, ct);
+    }
+
+    [Fact]
+    public async Task Handle_ShouldThrowException_WhenCadNotFound()
+    {
+        // Arrange
+        reads.SingleByIdAsync(id, true, ct).Returns(null as Cad);
+
+        ProductDeletedIntegrationEvent ie = new(
+            Id: default,
+            ImageId: default,
+            CadId: id
+        );
+        ProductDeletedIntegrationEventHandler handler = new(reads, writes, uow, storage);
+
+        // Assert
+        await Assert.ThrowsAsync<CadNotFoundException>(async () =>
+        {
+            // Act
+            await handler.Handle(ie);
+        });
+    }
+}
---
+++ CustomCADs.UnitTests.Files.Application/Images/SharedQueries/GetPresignedUrlPut/Data/GetImagePresignedUrlPutByIdHandlerValidData.cs
@@ -0,0 +1,12 @@
+﻿namespace CustomCADs.UnitTests.Files.Application.Images.SharedQueries.GetPresignedUrlPut.Data;
+
+using static ImagesData;
+
+public class GetImagePresignedUrlPutByIdHandlerValidData : GetImagePresignedUrlPutByIdHandlerData
+{
+    public GetImagePresignedUrlPutByIdHandlerValidData()
+    {
+        Add(ValidContentType1, ValidFileName1);
+        Add(ValidContentType2, ValidFileName2);
+    }
+}
