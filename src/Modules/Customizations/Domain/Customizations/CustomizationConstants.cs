12ab987afb2be825e446cc03ab0d541f09585645
Ivan Angelov <ivanangelov414@gmail.com>
2025-03-05 02:30:59 +0200

    Added Customizations Domain

---
+++ src/Modules/Customizations/Domain/Customizations/CustomizationConstants.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.RegularExpressions;
+
+namespace CustomCADs.Customizations.Domain.Customizations.Validation;
+
+public static partial class CustomizationConstants
+{
+    public const decimal ScaleMin = 1m;
+    public const decimal ScaleMax = 10m;
+    
+    public const decimal InfillMin = 0.2m;
+    public const decimal InfillMax = 1m;
+
+    public const decimal VolumeMin = 0;
+    public const decimal CostMin = 0m;
+    
+    /// <summary>
+    ///     In USD
+    /// </summary>
+    public const decimal ProfitMargin = 1.2m;
+    public const decimal WallFactor = 0.45m;
+
+    public static Regex Color => ColorRegex();
+
+    [GeneratedRegex(@"^#[0-9a-fA-F]{6}$", RegexOptions.Compiled)]
+    private static partial Regex ColorRegex();
+}
---
+++ src/Modules/Customizations/Domain/Customizations/Validation/CustomizationValidation.cs
@@ -0,0 +1,65 @@
+﻿using CustomCADs.Customizations.Domain.Common.Exceptions.Customizations;
+
+namespace CustomCADs.Customizations.Domain.Customizations.Validation;
+
+using static CustomizationConstants;
+
+public static class CustomizationValidation
+{
+    public static Customization ValidateScale(this Customization customization)
+    {
+        string property = "Scale";
+        decimal scale = customization.Scale;
+
+        if (scale < ScaleMin || scale > ScaleMax)
+        {
+            throw CustomizationValidationException.Range(property, ScaleMin, ScaleMax);
+        }
+
+        return customization;
+    }
+
+    public static Customization ValidateInfill(this Customization customization)
+    {
+        string property = "Infill";
+        decimal infill = customization.Infill;
+
+        if (infill < InfillMin || infill > InfillMax)
+        {
+            throw CustomizationValidationException.Range(property, InfillMin, InfillMax);
+        }
+
+        return customization;
+    }
+    
+    public static Customization ValidateVolume(this Customization customization)
+    {
+        string property = "Volume";
+        decimal volume = customization.Volume;
+
+        if (volume < VolumeMin)
+        {
+            throw CustomizationValidationException.Min(property, VolumeMin);
+        }
+
+        return customization;
+    }
+    
+    public static Customization ValidateColor(this Customization customization)
+    {
+        string property = "Color";
+        string color = customization.Color;
+
+        if (string.IsNullOrEmpty(color))
+        {
+            throw CustomizationValidationException.NotNull(property);
+        }
+
+        if (!Color.IsMatch(color))
+        {
+            throw CustomizationValidationException.Color();
+        }
+
+        return customization;
+    }
+}
