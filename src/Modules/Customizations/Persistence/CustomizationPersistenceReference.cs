aeff5f608206266f0b765e38aed2486044ccc1af
Ivan Angelov <ivanangelov414@gmail.com>
2025-03-05 02:46:17 +0200

    Added Customizations Persistence

---
+++ src/Modules/Customizations/Persistence/CustomizationPersistenceReference.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+
+namespace CustomCADs.Customizations.Persistence;
+
+public class CustomizationPersistenceReference
+{
+    public static Assembly Assembly => typeof(CustomizationPersistenceReference).Assembly;
+}
---
+++ src/Modules/Customizations/Persistence/Customizations/Configurations/CustomizationConfiguration.cs
@@ -0,0 +1,73 @@
+﻿using CustomCADs.Customizations.Domain.Customizations;
+using CustomCADs.Shared.Core.Common.TypedIds.Customizations;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CustomCADs.Customizations.Persistence.Customizations.Configurations;
+
+public class CustomizationConfiguration : IEntityTypeConfiguration<Customization>
+{
+    public void Configure(EntityTypeBuilder<Customization> builder)
+    {
+        builder
+            .SetPrimaryKey()
+            .SetStronglyTypedIds()
+            .SetValidations();
+    }
+}
+
+static class CustomizationConfigUtils
+{
+    public static EntityTypeBuilder<Customization> SetPrimaryKey(this EntityTypeBuilder<Customization> builder)
+    {
+        builder.HasKey(x => x.Id);
+
+        return builder;
+    }
+
+    public static EntityTypeBuilder<Customization> SetStronglyTypedIds(this EntityTypeBuilder<Customization> builder)
+    {
+        builder.Property(x => x.Id)
+            .ValueGeneratedOnAdd()
+            .HasConversion(
+                x => x.Value,
+                v => CustomizationId.New(v)
+            );
+
+        builder.Property(x => x.MaterialId)
+            .HasConversion(
+                x => x.Value,
+                v => MaterialId.New(v)
+            );
+
+        return builder;
+    }
+
+    public static EntityTypeBuilder<Customization> SetValidations(this EntityTypeBuilder<Customization> builder)
+    {
+        builder.Property(x => x.Scale)
+            .IsRequired()
+            .HasPrecision(4, 2)
+            .HasColumnName("Scale");
+
+        builder.Property(x => x.Infill)
+            .IsRequired()
+            .HasPrecision(4, 2)
+            .HasColumnName("Infill");
+
+        builder.Property(x => x.Volume)
+            .IsRequired()
+            .HasPrecision(18, 2)
+            .HasColumnName("Volume");
+
+        builder.Property(x => x.Color)
+            .IsRequired()
+            .HasMaxLength(7)
+            .HasColumnName("Color");
+
+        builder.Property(x => x.MaterialId)
+            .IsRequired()
+            .HasColumnName("MaterialId");
+
+        return builder;
+    }
+}
