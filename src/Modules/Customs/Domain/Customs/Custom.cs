e1a5223a9042227f5cc5e4f01d82f12cedde7d4b
Ivan Angelov <ivanangelov414@gmail.com>
2025-04-02 15:53:09 +0300

    Added Customs Domain

---
+++ src/Modules/Customs/Domain/Customs/Custom.cs
@@ -0,0 +1,174 @@
+﻿using CustomCADs.Customs.Domain.Customs.Entities;
+using CustomCADs.Customs.Domain.Customs.Enums;
+using CustomCADs.Customs.Domain.Customs.States;
+using CustomCADs.Customs.Domain.Customs.States.Implementations;
+using CustomCADs.Shared.Core.Bases.Entities;
+using CustomCADs.Shared.Core.Common.TypedIds.Accounts;
+using CustomCADs.Shared.Core.Common.TypedIds.Customizations;
+using CustomCADs.Shared.Core.Common.TypedIds.Delivery;
+using CustomCADs.Shared.Core.Common.TypedIds.Files;
+
+namespace CustomCADs.Customs.Domain.Customs;
+
+public class Custom : BaseAggregateRoot
+{
+    private ICustomState? state;
+    private ICustomState State => state ??= RestoreState();
+
+    private Custom() { }
+    private Custom(string name, string description, bool delivery, AccountId buyerId) : this()
+    {
+        Name = name;
+        Description = description;
+        OrderedAt = DateTimeOffset.UtcNow;
+        CustomStatus = CustomStatus.Pending;
+        BuyerId = buyerId;
+        ForDelivery = delivery;
+    }
+
+    public CustomId Id { get; init; }
+    public string Name { get; private set; } = string.Empty;
+    public string Description { get; private set; } = string.Empty;
+    public bool ForDelivery { get; private set; }
+    public CustomStatus CustomStatus { get; set; }
+    public DateTimeOffset OrderedAt { get; }
+    public AccountId BuyerId { get; private set; }
+    public AcceptedCustom? AcceptedCustom { get; private set; }
+    public FinishedCustom? FinishedCustom { get; private set; }
+    public CompletedCustom? CompletedCustom { get; private set; }
+
+    public static Custom Create(
+        string name,
+        string description,
+        bool forDelivery,
+        AccountId buyerId
+    ) => new Custom(name, description, forDelivery, buyerId)
+            .ValidateName()
+            .ValidateDescription();
+
+    public static Custom CreateWithId(
+        CustomId id,
+        string name,
+        string description,
+        bool delivery,
+        AccountId buyerId
+    ) => new Custom(name, description, delivery, buyerId)
+    {
+        Id = id
+    }
+    .ValidateName()
+    .ValidateDescription();
+
+    public Custom SetName(string name)
+    {
+        if (CustomStatus is not (CustomStatus.Pending or CustomStatus.Accepted or CustomStatus.Begun))
+        {
+            throw CustomValidationException<Custom>.Status(CustomStatus);
+        }
+
+        Name = name;
+        this.ValidateName();
+
+        return this;
+    }
+
+    public Custom SetDescription(string description)
+    {
+        if (CustomStatus is not (CustomStatus.Pending or CustomStatus.Accepted))
+        {
+            throw CustomValidationException<Custom>.Status(CustomStatus);
+        }
+
+        Description = description;
+        this.ValidateDescription();
+
+        return this;
+    }
+
+    public Custom SetDelivery(bool value)
+    {
+        if (CustomStatus is not (CustomStatus.Pending or CustomStatus.Accepted or CustomStatus.Begun or CustomStatus.Finished))
+        {
+            throw CustomValidationException<Custom>.Status(CustomStatus);
+        }
+        ForDelivery = value;
+
+        return this;
+    }
+
+    public Custom SetDesigner(AccountId designerId)
+    {
+        if (CustomStatus is not CustomStatus.Accepted)
+        {
+            throw CustomValidationException<Custom>.Status(CustomStatus);
+        }
+        AcceptedCustom!.SetDesignerId(designerId);
+
+        return this;
+    }
+
+    public Custom SetShipment(ShipmentId shipmentId)
+    {
+        if (CustomStatus is not CustomStatus.Completed)
+        {
+            throw CustomValidationException<Custom>.Status(CustomStatus);
+        }
+        CompletedCustom!.SetShipmentId(shipmentId);
+
+        return this;
+    }
+
+    public void Accept(AccountId designerId) => State.Accept(this, designerId);
+    public void Begin() => State.Begin(this);
+    public void Finish(CadId cadId, decimal price) => State.Finish(this, cadId, price);
+    public void Complete(CustomizationId? customizationId) => State.Complete(this, customizationId);
+    public void Cancel() => State.Cancel(this);
+    public void Report() => State.Report(this);
+
+    internal void SetState(ICustomState newState)
+    {
+        CustomStatus = newState.Status;
+        state = newState;
+    }
+
+    internal Custom ClearAcceptInfo()
+    {
+        AcceptedCustom = null;
+        return this;
+    }
+
+    internal Custom FillAcceptInfo(AccountId designerId)
+    {
+        AcceptedCustom = AcceptedCustom.Create(Id, designerId);
+        return this;
+    }
+
+    internal Custom FillFinishInfo(CadId cadId, decimal price)
+    {
+        FinishedCustom = FinishedCustom.Create(Id, price, cadId);
+        return this;
+    }
+
+    internal Custom FillCompleteInfo(CustomizationId? customizationId)
+    {
+        if (!ForDelivery)
+            CompletedCustom = CompletedCustom.Create(Id);
+        else if (customizationId is not null)
+            CompletedCustom = CompletedCustom.Create(Id, customizationId.Value);
+        else throw CustomValidationException<Custom>.Custom("You must provide a CustomizationId when completing a Custom for delivery.");
+
+        return this;
+    }
+
+    private ICustomState RestoreState()
+        => CustomStatus switch
+        {
+            CustomStatus.Pending => new PendingCustomState(),
+            CustomStatus.Accepted => new AcceptedCustomState(),
+            CustomStatus.Begun => new BegunCustomState(),
+            CustomStatus.Finished => new FinishedCustomState(),
+            CustomStatus.Completed => new CompletedCustomState(),
+            CustomStatus.Reported => new ReportedCustomState(),
+            _ => throw CustomValidationException<Custom>.Custom($"Unknown CustomStatus: {CustomStatus}")
+        };
+}
---
+++ src/Modules/Customs/Domain/Customs/Entities/Validations.cs
@@ -0,0 +1,20 @@
+﻿namespace CustomCADs.Customs.Domain.Customs.Entities;
+
+using static CustomConstants;
+
+public static class Validations
+{
+    public static FinishedCustom ValidatePrice(this FinishedCustom custom)
+    {
+        string property = "Price";
+        decimal? price = custom.Price;
+
+        decimal max = PriceMax, min = PriceMin;
+        if (price > max || price < min)
+        {
+            throw CustomValidationException<Custom>.Range(property, max, min);
+        }
+
+        return custom;
+    }
+}
