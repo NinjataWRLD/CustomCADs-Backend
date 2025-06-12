63eb5dc93dac07f16bf254569675c9cf9c31f167
Ivan Angelov <ivanangelov414@gmail.com>
2025-06-12 18:28:48 +0300

    Addded 'Payment Statuses' flows

---
+++ src/Modules/Carts/Application/PurchasedCarts/Queries/Internal/GetPaymentStatuses/GetPurchasedCartPaymentStatusesHandler.cs
@@ -0,0 +1,12 @@
+﻿using CustomCADs.Carts.Domain.PurchasedCarts.Enums;
+
+namespace CustomCADs.Carts.Application.PurchasedCarts.Queries.Internal.GetPaymentStatuses;
+
+public class GetPurchasedCartPaymentStatusesHandler
+	: IQueryHandler<GetPurchasedCartPaymentStatusesQuery, string[]>
+{
+	public Task<string[]> Handle(GetPurchasedCartPaymentStatusesQuery req, CancellationToken ct)
+		=> Task.FromResult(
+			Enum.GetNames<PaymentStatus>()
+		);
+}
---
+++ src/Modules/Customs/Application/Customs/Queries/Internal/Shared/GetPaymentStatuses/GetCustomPaymentStatusesHandler.cs
@@ -0,0 +1,12 @@
+﻿using CustomCADs.Customs.Domain.Customs.Enums;
+
+namespace CustomCADs.Customs.Application.Customs.Queries.Internal.Shared.GetPaymentStatuses;
+
+public class GetCustomPaymentStatusesHandler
+	: IQueryHandler<GetCustomPaymentStatusesQuery, string[]>
+{
+	public Task<string[]> Handle(GetCustomPaymentStatusesQuery req, CancellationToken ct)
+		=> Task.FromResult(
+			Enum.GetNames<PaymentStatus>()
+		);
+}
