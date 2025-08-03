ef63f28c60a0060d32189d2007318e602f54ec38
Ivan Angelov <ivanangelov414@gmail.com>
2025-08-03 16:44:56 +0300

    Added forgotten Command/Query abstraction restrictions

--- src/Shared/Abstractions/Requests/Commands/ICommandHandler.cs
+++ src/Shared/Abstractions/Requests/Commands/ICommandHandler.cs
@@ -1,11 +1,11 @@
 ﻿namespace CustomCADs.Shared.Abstractions.Requests.Commands;
 
-public interface ICommandHandler<in TCommand>
+public interface ICommandHandler<in TCommand> where TCommand : ICommand
 {
 	Task Handle(TCommand req, CancellationToken ct = default);
 }
 
-public interface ICommandHandler<in TCommand, TResponse>
+public interface ICommandHandler<in TCommand, TResponse> where TCommand : ICommand<TResponse>
 {
 	Task<TResponse> Handle(TCommand req, CancellationToken ct = default);
 }
